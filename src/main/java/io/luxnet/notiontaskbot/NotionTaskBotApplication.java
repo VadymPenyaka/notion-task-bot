4cd0a9ba6a52ab9e6fb0aca8f8aa731ea3cbb131
v.peniaka <v.peniaka@luxnet.io>
2026-02-15 20:23:15 +0200

    Initial commit

---
+++ src/main/java/io/luxnet/notiontaskbot/NotionTaskBotApplication.java
@@ -0,0 +1,13 @@
+package io.luxnet.notiontaskbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotionTaskBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotionTaskBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/luxnet/notiontaskbot/NotionTaskBotApplicationTests.java
@@ -0,0 +1,13 @@
+package io.luxnet.notiontaskbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotionTaskBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
